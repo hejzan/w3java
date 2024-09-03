edef5e49577402945fefe2a5b7c22360de6125df
hejzan <hejzan@gmail.com>
2024-09-03 22:16:42 +0200

    testing 123

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main { // filename must be the same name
+    public static void main(String[] args) { // main() method will execute anything g
+        System.out.println("Hello world");
+    }
+}
